55a687412c573abbfa3cfb4b605777cc91a0d67b
jarrodwhitley <jarrod@jarrodwhitley.com>
2024-07-12 16:41:10 -0500

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="bg-slate-800 text-white">Header</div>
+</template>
+
+<style scoped>
+
+</style>
