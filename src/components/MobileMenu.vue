cf6ab82aed5ac7c4964b1d161c083d446da0c99d
Jarrod Whitley <jarrodwhitley@gmail.com>
2024-07-15 11:15:10 -0500

    added sass / menu update

--- src/components/MobileMenu.vue
+++ src/components/MobileMenu.vue
@@ -10,18 +10,35 @@ const props = defineProps({
    <div class="absolute top-0 left-0 right-0 p-6 pb-4 text-white transition-all z-10 bg-center bg-cover"
         :class="showMenu ? 'max-h-full duration-[1000ms]' : 'max-h-16 duration-200'"
         :style="'background-image:url(' + [props.time === 'am' ? './src/assets/images/header_morning_bg.png' : '']">
-         <ul class="grid grid-rows-2 gap-4 mt-14"
-            :class="[showMenu ? 'opacity-100 transition-opacity-400' : 'opacity-0 transition-opacity-0']">
+         <ul class="grid grid-rows-2 gap-4 mt-14 transition-opacity"
+            :class="[showMenu ? 'opacity-100 duration-400' : 'opacity-0 duration-0']">
               <li class="text-2xl">
                     <a href="mailto:support@jarrodwhitley.com">🐞 Found a bug?</a>
               </li>
              <li class="text-2xl">
                  <a href="mailto:support@jarrodwhitley.com">☕️ Buy me a coffee </a>
              </li>
-             <!-- manually set font size to 10px-->
-             <li class="mt-5 text-gray-400">
+             <li class="mt-5">
                  Made with ❤️ by <a href="https://jarrodwhitley.com">Jarrod Whitley</a>
              </li>
          </ul>
    </div>
 </template>
+
+<style lang="scss" scoped>
+    $green: #315668;
+    #mobileMenu {
+        &.max-h-full {
+            &::before {
+                content: '';
+                position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                background: linear-gradient(to top, rgba($green, 0.9), transparent);
+                z-index: -1;
+            }
+        }
+    }
+</style>
