02166ce4e3979565caf085cb68f2d8fb2e04e63f
Jarrod Whitley <jarrod@jarrodwhitley.com>
2026-09-18 12:35:27 +0000

    Build m_and_e app with Docker

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/m_and_e/',
+    base: '/',
     root: './',
     server: {
         host: true,
